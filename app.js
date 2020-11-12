accb164f62c6da04ee6a1aaf701ec1303090efb1
Ankurkumarsaini <55224262+Ankurkumarsaini@users.noreply.github.com>
2020-11-12 15:01:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var logger = require('morgan');
 var Port = process.env.PORT || 8100;
 var app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 	res.status(200).send('Server is working.');
