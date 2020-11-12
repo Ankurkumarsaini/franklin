8a276801ff9747c6d7660f1e656ec00c56400f2b
Ankurkumarsaini <55224262+Ankurkumarsaini@users.noreply.github.com>
2020-11-12 15:50:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,20 @@
-// const createError = require('http-errors');
-var express = require('express');
-//const  path = require('path');
-//const  cookieParser = require('cookie-parser');
-//const logger = require('morgan');
+const express = require('express')
+// will use this later to send requests
+const http = require('http')
+const https = require('https')
+// import env variables
+require('dotenv').config()
 
-/*
-var indexRouter = require('./routes/index');
-var ideasRouter = require('./routes/ideas');
-var gcpTasksRouter = require('./routes/gcpTasks');
+const app = express()
+const port = process.env.PORT || 8200
 
-var demoRouter = require('./routes/demo');
-*/
-var app = express();
-
-/*
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-*/
-var Port = process.env.PORT || 8100;
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-	res.status(200).send('Server is working.');
-});
+	res.status(200).send('Server is working.')
+})
 
-app.listen(Port, () => {
-	console.log(`🌏 Server is running at http://localhost:${Port}`);
-});
-
-
-//app.use('/', indexRouter);
-//app.use('/ideas', ideasRouter);
-//app.use('/gcptasks', gcpTasksRouter);
-//app.use('/demo',demoRouter);
-
-/*
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-*/
-
-app.listen(Port,()=>{
-  console.log(`app listening on port ${Port}`);
-});
+app.listen(port, () => {
+	console.log(`🌏 Server is running at http://localhost:${port}`)
+})
