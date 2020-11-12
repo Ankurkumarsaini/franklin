50c84d2b08c916e8459336ee95318d0845a722ee
Ankurkumarsaini <55224262+Ankurkumarsaini@users.noreply.github.com>
2020-11-12 15:08:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const http = require('http')
+const https = require('https')
 var Port = process.env.PORT || 8100;
 var app = express();
 
